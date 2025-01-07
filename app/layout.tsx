e849c80f3f2f48c7790516f9e8b0d97464ccca5f
Harsh Mriduhash <Harshsahay2709@gmail.com>
2025-01-07 16:08:13 +0530

    Updating

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,43 +1,27 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
-
-import Header from '@/components/header'
-import Footer from '@/components/footer'
-import { ThemeProvider } from '@/components/theme-provider'
-
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata: Metadata = {
-  title: 'PinPoint AI',
-  description: 'AI tool for meetings'
-}
-
 export default function RootLayout({
-  children
-}: Readonly<{
+  children,
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
     <ClerkProvider>
-      <html
-        lang='en'
-        className='scroll-smooth antialiased'
-        suppressHydrationWarning
-      >
-        <body className={`flex min-h-screen flex-col ${inter.className}`}>
-          <ThemeProvider
-            enableSystem
-            attribute='class'
-            defaultTheme='system'
-            disableTransitionOnChange
-          >
-            <Header />
-            <main className='grow'>{children}</main>
-            <Footer />
-          </ThemeProvider>
+      <html lang="en">
+        <body>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          {children}
         </body>
       </html>
     </ClerkProvider>
--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,6 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(['/analyzer(.*)', '/forum(.*)'])
-
-export default clerkMiddleware(async (auth, req) => {
-  const { userId } = await auth()
-
-  if (!userId && isProtectedRoute(req)) {
-    // Add custom logic to run before redirecting
-
-    return auth().redirectToSignIn()
-  }
-})
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
@@ -19,4 +9,4 @@ export const config = {
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
-}
+};
